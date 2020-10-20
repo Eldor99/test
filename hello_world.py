1991ca81deb9811ca23ee2afbe71d43fa3de2e88
Eldor99 <eldor1999@protonmail.com>
2020-10-20 23:11:34 +0500

    test again

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+my_cities = ['Dallas','Tokyo','Seoul','Bangkok','Sydney']
+print(my_cities)
+print(sorted(my_cities))
+print(my_cities)
+print(my_cities.reverse())
+print(my_cities.reverse())
+my_cities.sort()
+print(my_cities)
